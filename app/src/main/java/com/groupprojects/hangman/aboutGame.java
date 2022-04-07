e8877974121d8d06e43df8f1eee0acc99b420049
Michael Gates <64144102+tahlly88@users.noreply.github.com>
2022-04-07 19:29:06 -0400

    added music to about page as a test, it continuously plays the music even after the back button is pressed though.

--- app/src/main/java/com/groupprojects/hangman/aboutGame.java
+++ app/src/main/java/com/groupprojects/hangman/aboutGame.java
@@ -1,7 +1,11 @@
 package com.groupprojects.hangman;
 
+import static com.groupprojects.hangman.R.raw.*;
+import static com.groupprojects.hangman.R.raw.about_theme;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -12,14 +16,15 @@ public class aboutGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        //the below code is to make sure the word list is working properly
 
-         Toast toast2 = Toast.makeText(this, wordList.getWord(), Toast.LENGTH_LONG);
-         toast2.show();
+        MediaPlayer media = MediaPlayer.create(this,about_theme);
+        media.start();
+
 
     }
 
     public void getNewWord(View view){
         Toast.makeText(aboutGame.this, wordList.getWord(), Toast.LENGTH_SHORT).show();
+
     }
 }
