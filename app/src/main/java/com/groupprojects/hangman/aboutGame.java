990a3c47088933409a29d136598d425f9d9e13de
Michael Gates <64144102+tahlly88@users.noreply.github.com>
2022-04-19 09:56:40 -0400

    Forgot to remove the test button in the about screen.

--- app/src/main/java/com/groupprojects/hangman/aboutGame.java
+++ app/src/main/java/com/groupprojects/hangman/aboutGame.java
@@ -12,14 +12,9 @@ public class aboutGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        //the below code is to make sure the word list is working properly
 
-         Toast toast2 = Toast.makeText(this, wordList.getWord(), Toast.LENGTH_LONG);
-         toast2.show();
 
     }
 
-    public void getNewWord(View view){
-        Toast.makeText(aboutGame.this, wordList.getWord(), Toast.LENGTH_SHORT).show();
-    }
+
 }
