2fd6f932a12fdc42ed5dfbaee0becffa85e01087
ajelle0077 <97971970+ajelle0077@users.noreply.github.com>
2022-04-25 17:17:34 -0400

    Add files via upload

---
+++ main/java/com/groupprojects/hangman/aboutGame.java
@@ -0,0 +1,20 @@
+package com.groupprojects.hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class aboutGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+
+    }
+
+
+}
